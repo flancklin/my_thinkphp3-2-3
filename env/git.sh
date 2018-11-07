9c970f6772b939cc2cb9ff733433c00fe32a0150
flancklin <475185283@qq.com>
2018-11-07 21:55:23 +0800

    Update git.sh

--- env/git.sh
+++ env/git.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /var/www/html/my_thinkphp3-2-3
 git pull origin master
-rm -rf /var/www/html/my_thinkphp3-2-3/code/Application/Runtime/
+rm -rf /var/www/html/my_thinkphp3-2-3/code/Application/Runtime/*
